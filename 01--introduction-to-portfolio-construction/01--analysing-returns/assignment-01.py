c15f2fab84c870be52b0d5bcf407e5a7bc230c3c
darkside <raghuram.shankar98@gmail.com>
2022-11-12 20:14:04 +0000

    format solutions

--- 01--introduction-to-portfolio-construction/01--analysing-returns/assignment-01.py
+++ 01--introduction-to-portfolio-construction/01--analysing-returns/assignment-01.py
@@ -14,13 +14,14 @@ df = pd.read_csv('01--introduction-to-portfolio-construction/data/Portfolios_For
 data = df.get(['Lo 20', 'Hi 20'])
 data = data/100
 total_returns_m = (data + 1).prod() - 1
-returns_y = (total_returns_m + 1)**(12/len(data)) - 1
-volatility_y = data.std() * np.sqrt(12)
+returns_y = ((total_returns_m + 1)**(12/len(data)) - 1) * 100
+volatility_y = (data.std() * np.sqrt(12))* 100
 
 
 """plot and print"""
-# print("Total monthly returns (%) = \n", total_returns_m * 100)
-print("Yearly returns (%) = \n", returns_y * 100)
-print("Yearly volatility = \n", volatility_y)
+print("1: Lo 20 Yearly returns =", returns_y["Lo 20"], "%")
+print("2: Lo 20 Yearly volatility =", volatility_y["Lo 20"], "%")
+print("3: Hi 20 Yearly returns =", returns_y["Hi 20"], "%")
+print("4: Hi 20 Yearly volatility =", volatility_y["Hi 20"], "%")
 data.plot()
 # plt.show()
